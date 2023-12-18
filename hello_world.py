5666f4cc363cbb4c330d0e8e2fac59f50c597cb9
adhavswapna <106309276+adhavswapna@users.noreply.github.com>
2023-12-19 03:47:58 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 def hello():
     return "Hello, World"
 
-<<<<<<< HEAD
+
 app.run('0.0.0.0', port=5000)
-=======
-app.run('0.0.0.0', port=5001)
->>>>>>> 2b73523 (some changes)
+
